4d350e65a4892c721327dd1dd3bcd69c1fc09569
raulsouth <raulsouth@gmail.com>
2025-04-12 23:23:42 -0300

    Agrego primeros archivos del curso

---
+++ hola_mundo.py
@@ -0,0 +1,8 @@
+#Hola mundo en Python.
+print('Hola mundo')
+print('Hola otra vez')
+
+#Tipos de datos
+print(type('hola'))
+print(type(2))
+print(type(2.3))
---
+++ variables.py
@@ -0,0 +1,11 @@
+#Variables
+#Es una forma de guardar datos
+
+a = 5
+b = 2
+c = a + b
+e = 'palabra'
+print(c)
+print(type(e))
+print(e, a)
+print('el número es:' , a)
